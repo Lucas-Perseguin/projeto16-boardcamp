477fe35558e5ecd20c3676560e7ba7c3c1011b90
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-12-08 00:55:26 -0300

    refact: change regexp

--- src/models/game.model.js
+++ src/models/game.model.js
@@ -1,8 +1,10 @@
 import Joi from 'joi';
 
 const gameModel = Joi.object({
-  name: Joi.string().required(),
-  image: Joi.string().pattern(new RegExp('(https?://)')).required(),
+  name: Joi.string().required,
+  image: Joi.string()
+    .pattern(new RegExp('https?://(www.)?[^/]*?/?([^$]*?$)?'))
+    .required(),
   stockTotal: Joi.number().integer().greater(0).required(),
   categoryId: Joi.number().integer().greater(0).required(),
   pricePerDay: Joi.number().integer().greater(0).required(),
