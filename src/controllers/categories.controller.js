19bb8e8b7427994db92c68f069333732368ae915
Lucas-Perseguin-Linux <lucasperseguin@gmail.com>
2022-12-07 23:04:52 -0300

    refact: return responses

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -3,9 +3,9 @@ import connection from '../database.js';
 export async function getCategories(req, res) {
   try {
     const categories = await connection.query('SELECT * FROM categories;');
-    res.send(categories.rows);
+    return res.send(categories.rows);
   } catch (err) {
-    res.sendStatus(500);
+    return res.sendStatus(500);
   }
 }
 
